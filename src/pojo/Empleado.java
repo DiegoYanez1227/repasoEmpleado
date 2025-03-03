375908103446bbcc21d9b9d78a7f31107c12d00e
DiegoYanez1227 <diegoyan0260@gmail.com>
2025-03-03 20:42:53 +0100

    commit

--- src/pojo/Empleado.java
+++ src/pojo/Empleado.java
@@ -1,5 +1,14 @@
 package pojo;
 
-public class Empleado {
+import java.sql.Date;
 
+public class Empleado {
+	private int id;
+	private String nombre;
+	private String puesto;
+	private Date fechaNacimiento;
+	private Departamento departamento;
+	
+	
+	
 }
